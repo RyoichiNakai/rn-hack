eac209eb22ba1fcea2db99e41a1a3baa8288eac2
RyoichiNakai <elmobasketball0913@me.com>
2022-05-18 01:13:13 +0900

    ℹ️ #4 フォント設定

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,6 +1,16 @@
 import React from 'react';
 import '../styles/global.scss';
 
+// Font
+import '@fontsource/josefin-sans/400.css';
+import '@fontsource/josefin-sans/500.css';
+import '@fontsource/noto-sans-jp/400.css';
+import '@fontsource/noto-sans-jp/500.css';
+
+// Font Awesome
+// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+// import { faRss } from '@fortawesome/free-solid-svg-icons'
+
 import Header from './Header';
 
 const Layout: React.FC = ({ children }) => {
